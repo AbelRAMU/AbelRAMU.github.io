84da76c2f1e50495e0d496bcd8537fa8dac196ec
Abel <159206481+AbelRAMU@users.noreply.github.com>
2024-08-30 18:44:50 -0700

    Practica 1 nicios en php 30/08/2024

---
+++ BaseDeDatosWeb/Parcial1/HolaMundo.php
@@ -0,0 +1,27 @@
+<?php
+
+    $nombre = "AbelitoGamer";
+
+    $numero = 17;
+
+    $numero2 = "34";
+
+
+    $total = $numero + $numero2;
+
+    echo "TOTAL = " . $total;
+
+    echo "<h1>Hola mundo</h1>";
+
+    
+    for($i=0; $i<=10; $i++){
+
+        echo $i . "<br>";
+
+    }
+
+
+    echo "<br>Abel Ramirez Urias";
+    
+
+?>
