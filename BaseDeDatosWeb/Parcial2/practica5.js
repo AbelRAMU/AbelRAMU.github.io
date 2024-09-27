90d36ed40ffd468161486402cbfacc8d6a6a8a3c
Abel <159206481+AbelRAMU@users.noreply.github.com>
2024-09-26 17:31:55 -0700

    Practica 5 25/09/24 (26/09/24)
    Apoco se subia esto ajajajjaj

---
+++ BaseDeDatosWeb/Parcial2/practica5.js
@@ -0,0 +1,19 @@
+console.log("Hola mundoO")
+
+var cambio = false;
+
+function cambia_color(){
+    if(cambio){
+        document.getElementById("cuadro").style.backgroundColor = 'white';
+        cambio=false
+    }else{
+        document.getElementById("cuadro").style.backgroundColor = 'green';
+        cambio=true;
+    }
+}
+function ocultar(){
+    document.getElementById("titulo").style.display = 'none';
+}
+function mostrar(){
+    document.getElementById("titulo").style.display = '';
+}
